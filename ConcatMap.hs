586d06725a32fa4f8d34febd9459fe6f8f282f91
Joey Adams <joeyadams3.14159@gmail.com>
2012-03-15 21:35:42 -0400

    Have WB build up the write size, as is done in Builder
    Now it's back to being as fast as it should be.

--- ConcatMap.hs
+++ ConcatMap.hs
@@ -19,29 +19,24 @@ import System.IO.Unsafe         (unsafePerformIO)
 import qualified Data.ByteString        as B
 import qualified Data.ByteString.Unsafe as U
 
-newtype WB = WB { runWB :: Ptr Word8 -> Ptr Word8 -> IO (Maybe (Ptr Word8)) }
+data WB = WB { runWB    :: !(Ptr Word8 -> IO (Ptr Word8))
+             , lengthWB :: !Int
+             }
     deriving Typeable
 
 instance Monoid WB where
-    mempty = WB $ \_we wp -> return $ Just wp
+    mempty = WB return 0
     {-# INLINE mempty #-}
 
-    a `mappend` b = WB $ \we wp -> do
-        m <- runWB a we wp
-        case m of
-            Nothing  -> return Nothing
-            Just wp' -> runWB b we wp'
+    a `mappend` b = WB (runWB a >=> runWB b) (lengthWB a + lengthWB b)
     {-# INLINE mappend #-}
 
 wb :: Word8 -> WB
-wb b = WB $ \we wp ->
-    if wp < we
-        then do
-            poke wp b
-            let !wp' = wp `plusPtr` 1
-            return $ Just wp'
-        else
-            return Nothing
+wb b = WB { runWB = \wp -> do
+                poke wp b
+                return $! (wp `plusPtr` 1)
+          , lengthWB = 1
+          }
 {-# INLINE wb #-}
 
 type Convert = Ptr Word8
@@ -55,11 +50,13 @@ concatMapBuf f = \re we rp0 wp0 ->
     let loop !rp !wp | rp >= re  = return $ Just wp
                      | otherwise = do
             b <- peek rp
-            let !rp1 = rp `plusPtr` 1
-            m <- runWB (f b) we wp
-            case m of
-                Nothing  -> return Nothing
-                Just wp' -> loop rp1 wp'
+            let !rp' = rp `plusPtr` 1
+                WB r n = f b
+            if (we `minusPtr` wp) < n
+                then return Nothing
+                else do
+                    wp' <- r wp
+                    loop rp' wp'
      in loop rp0 wp0
 {-# INLINE concatMapBuf #-}
 
