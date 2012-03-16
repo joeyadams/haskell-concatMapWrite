67adfe669546bd3525670fcbae666abbcede7ba6
Joey Adams <joeyadams3.14159@gmail.com>
2012-03-16 01:06:17 -0400

    Clean up escapeCopyBytea formatting

--- escape-bytea-concatMap.hs
+++ escape-bytea-concatMap.hs
@@ -3,26 +3,30 @@ import ConcatMapWrite
 import Blaze.ByteString.Builder
 import Data.Bits
 import Data.ByteString          (ByteString)
-import Data.ByteString.Char8    ()
 import Data.ByteString.Internal (c2w)
 import Data.Monoid
+import Data.Word                (Word8)
 
 import qualified Data.ByteString.Lazy   as L
 
 escapeCopyBytea :: ByteString -> ByteString
-escapeCopyBytea = concatMapWrite f
-    where
-        f c | c == c2w '\\' = writeWord8 92
-                    `mappend` writeWord8 92
-                    `mappend` writeWord8 92
-                    `mappend` writeWord8 92
-        f c | c >= 32 && c <= 126 = writeWord8 c
-        f c =             writeWord8 (c2w '\\')
-                `mappend` writeWord8 (c2w '\\')
-                `mappend` (writeWord8 $ c2w '0' + ((c `shiftR` 6) .&. 0x7))
-                `mappend` (writeWord8 $ c2w '0' + ((c `shiftR` 3) .&. 0x7))
-                `mappend` (writeWord8 $ c2w '0' + (c .&. 0x7))
+escapeCopyBytea = concatMapWrite escape1
 {-# INLINE escapeCopyBytea #-}
+
+escape1 :: Word8 -> Write
+escape1 c | c == c2w '\\'
+          = writeWord8 (c2w '\\') `mappend`
+            writeWord8 (c2w '\\') `mappend`
+            writeWord8 (c2w '\\') `mappend`
+            writeWord8 (c2w '\\')
+          | c >= 32 && c <= 126
+          = writeWord8 c
+          | otherwise
+          = writeWord8 (c2w '\\') `mappend`
+            writeWord8 (c2w '\\') `mappend`
+            (writeWord8 $ c2w '0' + ((c `shiftR` 6) .&. 0x7)) `mappend`
+            (writeWord8 $ c2w '0' + ((c `shiftR` 3) .&. 0x7)) `mappend`
+            (writeWord8 $ c2w '0' + (c .&. 0x7))
 
 mapChunks :: (ByteString -> ByteString) -> L.ByteString -> L.ByteString
 mapChunks f = L.fromChunks . map f . L.toChunks
