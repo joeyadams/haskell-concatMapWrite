50f7b28f50130f69397de629e96cadacd03b90fc
Joey Adams <joeyadams3.14159@gmail.com>
2012-03-16 01:14:34 -0400

    escapeCopyBytea.c: Cleaned up a bit, and added BSD-3 license

--- escapeCopyBytea.c
+++ escapeCopyBytea.c
@@ -1,11 +1,42 @@
+/*
+ * Copyright (c) 2012, Joseph Adams
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Joseph Adams nor the names of other
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <err.h>
 #include <stdio.h>
 
 /*
- * Like c_escape_copy_text, but escape the datum so it will be suitable for
- * PostgreSQL's BYTEA input function.  Note that this does not use the hex
- * format introduced by PostgreSQL 9.0, as it is readable only by
- * PostgreSQL 9.0 and up.
+ * Escape binary content so it will be suitable for PostgreSQL's BYTEA input
+ * function.  Note that this does not use the hex format introduced by
+ * PostgreSQL 9.0, as it is readable only by PostgreSQL 9.0 and up.
  *
  * This performs two escape operations:
  *
@@ -16,7 +47,7 @@
  *
  * The buffer pointed to by @out should be at least 5*in_size bytes long.
  */
-unsigned char *c_escape_copy_bytea(const unsigned char *in, int in_size, unsigned char *out)
+unsigned char *escapeCopyBytea(const unsigned char *in, int in_size, unsigned char *out)
 {
     while (in_size-- > 0) {
         unsigned char c = *in++;
@@ -75,7 +106,7 @@ int main(void)
             break;
         }
 
-        writelen = (char*)c_escape_copy_bytea((const unsigned char *) in, readlen, (unsigned char *) out) - out;
+        writelen = (char*)escapeCopyBytea((const unsigned char *) in, readlen, (unsigned char *) out) - out;
 
         wrotelen = fwrite(out, 1, writelen, stdout);
         if (wrotelen != writelen)
